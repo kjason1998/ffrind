0d3c24c5d7a5f409bfcb846187704e9651834ff2
kjason1998 <kjason1998@gmail.com>
2020-01-18 18:57:45 +0000

    test

--- app/src/main/java/com/kevex/ffriend/MapsActivity.java
+++ app/src/main/java/com/kevex/ffriend/MapsActivity.java
@@ -1,30 +1,143 @@
 package com.kevex.ffriend;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.animation.IntEvaluator;
+import android.animation.ValueAnimator;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Locale;
 
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnPoiClickListener {
+    private final String TAG = "MapsActivity";
+
+    private final int locationRequestCode = 1000;
+
+    private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap mMap;
+
+
+    private double wayLatitude = 0.0, wayLongitude = 0.0;
+
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setContentView(R.layout.activity_maps);// Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mapFragment.getMapAsync(this);
+
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    locationRequestCode);
+
+        } else {
+            // already permission granted
+            // get location here
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        wayLatitude = location.getLatitude();
+                        wayLongitude = location.getLongitude();
+                        Toast.makeText(MapsActivity.this.getApplicationContext(), "Oncreate"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        // HEREEEEEEEEEEEEEEEEEE
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    Toast.makeText(MapsActivity.this.getApplicationContext(), "locationCallback location is null"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        wayLatitude = location.getLatitude();
+                        wayLongitude = location.getLongitude();
+                        Toast.makeText(MapsActivity.this.getApplicationContext(), "locationCallback"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        };
+
+        //remove
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(locationCallback);
+        }
     }
 
+
+    //handle when after asking permission finish
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            // 1000 -> location request code
+            case 1000: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                wayLatitude = location.getLatitude();
+                                wayLongitude = location.getLongitude();
+                                Toast.makeText(MapsActivity.this.getApplicationContext(), "onRequestPermissionsResult"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +151,86 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined in /res/raw folder
+            // Map design is from https://mapstyle.withgoogle.com/
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        mMap.setOnPoiClickListener(this);
         // Add a marker in Sydney and move the camera
         LatLng Swansea = new LatLng(51.621441, -3.943646);
-        mMap.addMarker(new MarkerOptions().position(Swansea).title("Initial marke swansea"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Swansea));
+        MarkerOptions marker = new MarkerOptions().position(Swansea)
+                .title("Initial marke swansea")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(51.62, -3.94))
+                .radius(10)
+                .strokeColor(Color.WHITE)
+                .fillColor(Color.BLUE)
+                .clickable(true));
+
+        mMap.setOnCircleClickListener(onClickCircleListener());
+
+        mMap.addMarker(marker);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Swansea, 15));
+
+        if (mMap != null) {
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location arg0) {
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(arg0.getLatitude(), arg0.getLongitude()),15));
+                    Toast.makeText(getApplicationContext(),"Location is change",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
+
+    private GoogleMap.OnCircleClickListener onClickCircleListener() {
+        return new GoogleMap.OnCircleClickListener() {
+            @Override
+            public void onCircleClick(Circle circle) {
+                Toast.makeText(getApplicationContext(),"id is" + circle.getId(),Toast.LENGTH_SHORT).show();
+                animateCircle(circle);
+            }
+        };
+    }
+
+    public void animateCircle(final Circle circle){
+        ValueAnimator vAnimator = ValueAnimator.ofInt(9,10);
+        //vAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        vAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        vAnimator.setEvaluator(new IntEvaluator());
+        vAnimator.setDuration(150);
+
+        vAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        vAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int animatedValue = (Integer)valueAnimator.getAnimatedValue();
+                circle.setRadius(animatedValue);
+            }
+        });
+        vAnimator.start();
+        Toast.makeText(getApplicationContext(),"Finish animation",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        Toast.makeText(getApplicationContext(), "Clicked: " +
+                        poi.name + "\nPlace ID:" + poi.placeId +
+                        "\nLatitude:" + poi.latLng.latitude +
+                        " Longitude:" + poi.latLng.longitude,
+                Toast.LENGTH_SHORT).show();
+    }
+
 }
