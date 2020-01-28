151f180f786a8e7f57ecfe9c2e69e3bdfd366e61
kjason1998 <kjason1998@gmail.com>
2020-01-28 22:30:24 +0000

    after meeting on tuesday

--- app/src/main/java/com/kevex/ffriend/MapsActivity.java
+++ app/src/main/java/com/kevex/ffriend/MapsActivity.java
@@ -10,7 +10,6 @@ import android.animation.ValueAnimator;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,27 +18,27 @@ import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnPoiClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
     private final String TAG = "MapsActivity";
 
     private final int locationRequestCode = 1000;
@@ -47,11 +46,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap mMap;
 
+    private boolean mLocationPermissionGranted;
 
     private double wayLatitude = 0.0, wayLongitude = 0.0;
-
-    private LocationRequest locationRequest;
-    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,58 +57,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocationPermission();
         mapFragment.getMapAsync(this);
-
-        // check permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //ask for permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    locationRequestCode);
-
-        } else {
-            // already permission granted
-            // get location here
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        wayLatitude = location.getLatitude();
-                        wayLongitude = location.getLongitude();
-                        Toast.makeText(MapsActivity.this.getApplicationContext(), "Oncreate"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-        // HEREEEEEEEEEEEEEEEEEE
-        locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(1000);
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    Toast.makeText(MapsActivity.this.getApplicationContext(), "locationCallback location is null"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    if (location != null) {
-                        wayLatitude = location.getLatitude();
-                        wayLongitude = location.getLongitude();
-                        Toast.makeText(MapsActivity.this.getApplicationContext(), "locationCallback"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        };
-
-        //remove
-        if (mFusedLocationClient != null) {
-            mFusedLocationClient.removeLocationUpdates(locationCallback);
-        }
     }
 
+    /**
+     * Manipulates the map, once available.
+     * This callback is triggered when the map is ready to be used.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // check permission
+        mMap = googleMap;
+        setGoogleMapStyles(googleMap);
+        // Add a marker circle marker around swansea
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(51.62, -3.94))
+                .radius(10)
+                .strokeWidth(10)
+                .strokeColor(Color.WHITE)
+                .fillColor(Color.BLUE)
+                .clickable(true));
 
-    //handle when after asking permission finish
+        mMap.setOnCircleClickListener(onClickCircleListener());
+        mMap.setMyLocationEnabled(true);
+    }
+
+    /**
+     * handle when after asking permission finish
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -139,18 +116,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "location change", Toast.LENGTH_SHORT).show();
+//        mCurrentLocation = location;
+//        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+//        moveUser(Location location);
+    }
+
+
+    /**
+     * Circle marker on click handler
+     */
+    private GoogleMap.OnCircleClickListener onClickCircleListener() {
+        return new GoogleMap.OnCircleClickListener() {
+            @Override
+            public void onCircleClick(Circle circle) {
+                Toast.makeText(getApplicationContext(),"id is" + circle.getId(),Toast.LENGTH_SHORT).show();
+                animateCircle(circle);
+            }
+        };
+    }
+
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    locationRequestCode);
+
+        } else {
+            mLocationPermissionGranted = true;
+            // already permission granted
+            // get location here
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        wayLatitude = location.getLatitude();
+                        wayLongitude = location.getLongitude();
+                        Toast.makeText(MapsActivity.this.getApplicationContext(), "getLocationpermission"+String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude), Toast.LENGTH_SHORT).show();
+                        getDeviceLocation();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Gets the current location of the device, and positions the map's camera.
+     */
+    private void getDeviceLocation() {
+        /**
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),"getDevicelocationMLocationGrantedTaskSuccess",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(),""+task.getResult().getLatitude()+" "+task.getResult().getLongitude(),Toast.LENGTH_LONG).show();
+                            // Set the map's camera position to the current location of the device.
+                            //mLastKnownLocation = task.getResult(); delete this
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(task.getResult().getLatitude(),
+                                            task.getResult().getLongitude()), 15));
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    /**
+     * Styling the google map
+     * @param googleMap
+     */
+    private void setGoogleMapStyles(GoogleMap googleMap) {
         try {
             // Customise the styling of the base map using a JSON object defined in /res/raw folder
             // Map design is from https://mapstyle.withgoogle.com/
@@ -164,47 +223,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (Resources.NotFoundException e) {
             Log.e(TAG, "Can't find style. Error: ", e);
         }
-
-        mMap.setOnPoiClickListener(this);
-        // Add a marker in Sydney and move the camera
-        LatLng Swansea = new LatLng(51.621441, -3.943646);
-        MarkerOptions marker = new MarkerOptions().position(Swansea)
-                .title("Initial marke swansea")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-        Circle circle = mMap.addCircle(new CircleOptions()
-                .center(new LatLng(51.62, -3.94))
-                .radius(10)
-                .strokeColor(Color.WHITE)
-                .fillColor(Color.BLUE)
-                .clickable(true));
-
-        mMap.setOnCircleClickListener(onClickCircleListener());
-
-        mMap.addMarker(marker);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Swansea, 15));
-
-        if (mMap != null) {
-            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-                @Override
-                public void onMyLocationChange(Location arg0) {
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(arg0.getLatitude(), arg0.getLongitude()),15));
-                    Toast.makeText(getApplicationContext(),"Location is change",Toast.LENGTH_LONG).show();
-                }
-            });
-        }
     }
 
-    private GoogleMap.OnCircleClickListener onClickCircleListener() {
-        return new GoogleMap.OnCircleClickListener() {
-            @Override
-            public void onCircleClick(Circle circle) {
-                Toast.makeText(getApplicationContext(),"id is" + circle.getId(),Toast.LENGTH_SHORT).show();
-                animateCircle(circle);
-            }
-        };
-    }
-
+    /**
+     * animation clicked for circle
+     * @param circle
+     */
     public void animateCircle(final Circle circle){
         ValueAnimator vAnimator = ValueAnimator.ofInt(9,10);
         //vAnimator.setRepeatCount(ValueAnimator.INFINITE);
@@ -223,14 +247,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         vAnimator.start();
         Toast.makeText(getApplicationContext(),"Finish animation",Toast.LENGTH_LONG).show();
     }
-
-    @Override
-    public void onPoiClick(PointOfInterest poi) {
-        Toast.makeText(getApplicationContext(), "Clicked: " +
-                        poi.name + "\nPlace ID:" + poi.placeId +
-                        "\nLatitude:" + poi.latLng.latitude +
-                        " Longitude:" + poi.latLng.longitude,
-                Toast.LENGTH_SHORT).show();
-    }
-
 }
