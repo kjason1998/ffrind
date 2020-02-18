5331f87247d375f4ee80e256ab13e7559ab5db2e
danjhart123 <danjhart1@hotmail.co.uk>
2020-02-18 20:56:00 +0000

    Firebase authenticate working for login and register

--- app/src/main/java/com/kevex/ffriend/LoginActivity.java
+++ app/src/main/java/com/kevex/ffriend/LoginActivity.java
@@ -1,14 +1,54 @@
 package com.kevex.ffriend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    String email;
+    String password;
+    private FirebaseAuth userAuthenticate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        userAuthenticate = FirebaseAuth.getInstance();
+        email = "Testing1@hotmail.com";
+        password = "Testing";
     }
+
+    public void changeToRegister(View view) {
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
+
+    public void login(View view) {
+        userAuthenticate.signOut();
+        userAuthenticate.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Success",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Failed",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/kevex/ffriend/RegisterActivity.java
+++ app/src/main/java/com/kevex/ffriend/RegisterActivity.java
@@ -1,14 +1,60 @@
 package com.kevex.ffriend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
+    String email;
+    String password;
+    private FirebaseAuth userAuthenticate;
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = userAuthenticate.getCurrentUser();
+        if(currentUser != null) {
+            System.out.println(currentUser.toString());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        userAuthenticate = FirebaseAuth.getInstance();
+        email = "Testing1@hotmail.com";
+        password = "Testing";
+
+    }
+
+
+    public void register(View view){
+
+        userAuthenticate.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Success",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 }
