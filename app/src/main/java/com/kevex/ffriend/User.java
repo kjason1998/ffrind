88a30d75dff68ec77303f8e0716b1b6bc7860e32
danjhart123 <danjhart1@hotmail.co.uk>
2020-02-18 13:53:12 +0000

    User class model implemented, constructors, getters, setters and to string

--- app/src/main/java/com/kevex/ffriend/User.java
+++ app/src/main/java/com/kevex/ffriend/User.java
@@ -1,4 +1,80 @@
 package com.kevex.ffriend;
 
 public class User {
+
+    String email;
+    String password;
+    String username;
+    int userID;
+    String phoneNumber;
+
+    public User(){
+
+    }
+
+    public User(String email, String password, String username, String phoneNumber){
+        setEmail(email);
+        setPassword(password);
+        setUsername(username);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public User(int userID, String email, String password, String username, String phoneNumber){
+        setUserID(userID);
+        setEmail(email);
+        setPassword(password);
+        setUsername(username);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + this.getEmail() + '\'' +
+                ", password='" + this.getPassword() + '\'' +
+                ", username='" + this.getUsername() + '\'' +
+                ", userID=" + this.getUserID() + '\'' +
+                ", phoneNumber='" + this.getPhoneNumber() + '\'' +
+                '}';
+    }
 }
